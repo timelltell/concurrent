7300a822f339eab83e697a1688cf174e22d884ba
timellchen <timellchen@tencent.com>
2021-03-13 22:09:40 +0800

    1

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "Concurrent/Simple"
+
+func main(){
+	//Simple.Run()
+	//Simple.Channel_run()
+	//Simple.Channel_run2()
+	//Simple.Channnel_run3()
+	//Simple.Wait_group()
+	Simple.Product_Consumer()
+}
